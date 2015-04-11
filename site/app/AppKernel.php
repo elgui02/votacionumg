3f4187f99a2820ecf3534f1bc4ad674d10595833
Denevy <d3n3vy@gmail.com>
2015-04-11 01:21:18 -0600

    Agregando DataTables

--- site/app/AppKernel.php
+++ site/app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Umg\VotacionBundle\UmgVotacionBundle(),
+            new Brown298\DataTablesBundle\Brown298DataTablesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
