abac8d3ba6402864c93c192bdbb8c52a801fc1a5
Guillermo Arana <garana@solucionestip.com>
2015-04-20 00:46:11 +0000

    Agregando bundle para gráficas

--- site/src/Umg/VotacionBundle/Controller/DefaultController.php
+++ site/src/Umg/VotacionBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Umg\VotacionBundle\Entity\Evaluacion;
 use Umg\VotacionBundle\Entity\Respuestum;
 use Umg\VotacionBundle\Entity\Opcion;
-
+use Ob\HighchartsBundle\Highcharts\Highchart;
 
 class DefaultController extends Controller
 {
@@ -140,5 +140,30 @@ class DefaultController extends Controller
             $em->getConnection()->rollback();
             throw $e;
         }
+    }
+
+    public function chartAction()
+    {
+        $ob = new Highchart();
+        $ob->chart->renderTo('piechart');
+        $ob->title->text('Browser market shares at a specific website in 2010');
+        $ob->plotOptions->pie(array(
+            'allowPointSelect'  => true,
+            'cursor'    => 'pointer',
+            'dataLabels'    => array('enabled' => false),
+            'showInLegend'  => true
+        ));
+        $data = array(
+            array('Firefox', 45.0),
+            array('IE', 26.8),
+            array('Chrome', 12.8),
+            array('Safari', 8.5),
+            array('Opera', 6.2),
+            array('Others', 0.7),
+        );
+        $ob->series(array(array('type' => 'pie','name' => 'Browser share', 'data' => $data))); 
+        return $this->render('UmgVotacionBundle:Default:chart.html.twig', array(
+            'chart' => $ob
+        ));
     }
 }
